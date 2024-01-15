3d3016a22036d644b0292c547d275695cf1fc73b
Beber <94356405+Beber328@users.noreply.github.com>
2024-01-15 10:06:03 +0100

    Create date.py

---
+++ date.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+# Obtenez la date et l'heure actuelles
+date_actuelle = datetime.now()
+
+# Affichez la date et l'heure actuelles
+print("Date et heure actuelles :", date_actuelle)
