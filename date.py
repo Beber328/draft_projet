2a6905f4c91e2f15db97d239227557374866cfc3
Beber <94356405+Beber328@users.noreply.github.com>
2024-01-16 09:45:04 +0100

    Update date.py

--- date.py
+++ date.py
@@ -1,5 +1,5 @@
+from datetime import datetime
 import sys
-import codecs
 
 def check_date_and_challenges(selected_challenges):
     # Obtenez la date et l'heure actuelles
@@ -11,7 +11,7 @@ def check_date_and_challenges(selected_challenges):
     # Comparez la date actuelle avec la date limite
     if date_actuelle > date_limite:
         # Vérifiez le nombre de challenges sélectionnés
-        if selected_challenges > 8:
+        if len(selected_challenges.split(',')) > 8:
             print("Dans les temps")
         else:
             print("En retard sur les challenges")
@@ -21,11 +21,11 @@ def check_date_and_challenges(selected_challenges):
 if __name__ == "__main__":
     # Vérifiez si le nombre d'arguments est correct
     if len(sys.argv) != 2:
-        print("Veuillez fournir le nombre de challenges sélectionnés en tant qu'argument.")
+        print("Veuillez fournir les challenges sélectionnés en tant qu'argument.")
         sys.exit(1)
 
-    # Récupérez le nombre de challenges sélectionnés depuis les arguments de la ligne de commande
-    selected_challenges = int(sys.argv[1])
+    # Récupérez les challenges sélectionnés depuis les arguments de la ligne de commande
+    selected_challenges = sys.argv[1]
 
     # Vérifiez la date et le nombre de challenges
     check_date_and_challenges(selected_challenges)
